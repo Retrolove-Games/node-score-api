f4fd1dd6943366c7a81d711c7bb0b437298d7d59
Konrad Fedorczyk <contact@realhe.ro>
2021-03-14 22:56:02 +0100

    Add express with hello world server

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,10 @@
-const message = "Hello World";
-console.log(message);
+var express = require('express');
+var app = express();
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
+
+app.get('/url', (req, res, next) => {
+  res.json(['Tony', 'Lisa', 'Michael', 'Ginger', 'Food']);
+});
