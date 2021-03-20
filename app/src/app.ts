e29caf6a1677801a997140254b1d7bb6f1e628cd
Konrad Fedorczyk <contact@realhe.ro>
2021-03-20 19:55:15 +0100

    Move app files to directory

--- app/src/app.ts
+++ app/src/app.ts
@@ -5,6 +5,6 @@ app.listen(3000, () => {
   console.log('Server running on port 3000');
 });
 
-app.get('/url', (req, res, next) => {
+app.get('/', (req, res, next) => {
   res.json(['Tony', 'Lisa', 'Michael', 'Ginger', 'Food']);
 });
