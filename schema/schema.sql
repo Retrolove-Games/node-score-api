737d5c09e6fcbd0bfcb173058f019b157c1482cc
Konrad Fedorczyk <contact@realhe.ro>
2021-03-22 23:04:51 +0100

    Add initial schema

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,6 +1,17 @@
 USE score_api;
 
-CREATE TABLE hello_world(
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    hello VARCHAR(255)
+CREATE TABLE `projects` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL
 );
+
+CREATE TABLE `highscores` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `projectId` int,
+  `nickname` varchar(255) NOT NULL,
+  `score` int NOT NULL DEFAULT 0,
+  `source` varchar(100),
+  `checksum` char(32) NOT NULL
+);
+
+ALTER TABLE `highscores` ADD FOREIGN KEY (`projectId`) REFERENCES `projects` (`id`);
