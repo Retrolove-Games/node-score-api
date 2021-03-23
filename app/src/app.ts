3767a79968ef26739741e8019832fdb6feb1999d
Konrad Fedorczyk <contact@realhe.ro>
2021-03-23 19:07:37 +0100

    Add connection test

--- app/src/app.ts
+++ app/src/app.ts
@@ -2,12 +2,21 @@ import db from "./db/sequelize";
 var express = require('express');
 var app = express();
 
+(async () => {
+  try {
+    await db.sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
+
 app.listen(3000, () => {
   console.log('Server running on port 3000');
 });
 
 app.get('/', (req, res, next) => {
-  res.json({hello: "world!"});
+  res.json({hello: "world!!!", current_env: process.env.NODE_ENV});
 });
 
 app.get('/db', (req, res, next) => {
