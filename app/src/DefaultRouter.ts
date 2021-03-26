7c8fc3f9cfb310be325cac2b367901e383fbdcd4
Konrad Fedorczyk <fedek6@gmail.com>
2021-03-26 10:18:04 +0100

    Fix path.join call

--- app/src/DefaultRouter.ts
+++ app/src/DefaultRouter.ts
@@ -1,12 +1,11 @@
 import { Router } from "express";
 import path from "path";
 
-
 const DefaultRouter = Router();
 
 // Main page
 DefaultRouter.get("/", (request, response) => {
-  response.sendFile(path.join(__dirname + '/../public/index.html'));
+  response.sendFile(path.join(__dirname, "/../public/index.html"));
 });
 
 export default DefaultRouter;
