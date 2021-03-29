924b6ca67df8e29e3285913cc3bee916e644677d
Konrad Fedorczyk <fedek6@gmail.com>
2021-03-29 18:31:27 +0200

    Update schema

--- schema/schema.sql
+++ schema/schema.sql
@@ -3,15 +3,16 @@ USE score_api;
 CREATE TABLE `projects` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
-  `encryption_key` varchar(255) NOT NULL 
+  `encryption_key` varchar(255) NOT NULL
 );
 
 CREATE TABLE `highscores` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
-  `projectId` int,
+  `project_id` int,
   `nickname` varchar(255) NOT NULL,
   `score` int NOT NULL DEFAULT 0,
   `source` varchar(100)
 );
 
-ALTER TABLE `highscores` ADD FOREIGN KEY (`projectId`) REFERENCES `projects` (`id`);
+ALTER TABLE `highscores` ADD FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`);
+
