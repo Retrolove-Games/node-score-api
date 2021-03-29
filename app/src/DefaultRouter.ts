f5679909c162d78cc3a179bc52dc5c4ce7055ca2
Konrad Fedorczyk <fedek6@gmail.com>
2021-03-29 23:50:19 +0200

    Add route demo

--- app/src/DefaultRouter.ts
+++ app/src/DefaultRouter.ts
@@ -8,4 +8,10 @@ DefaultRouter.get("/", (request, response) => {
   response.sendFile(path.join(__dirname, "/../public/index.html"));
 });
 
+DefaultRouter.post("/score/:id", (request, response) => {
+  console.log(request.body);
+  console.log(request.params.id);
+  response.send(request.body);
+});
+
 export default DefaultRouter;
