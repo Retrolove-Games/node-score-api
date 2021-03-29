223e2b4e081089f99d30f51b3770bcdb5ac25fdf
Konrad Fedorczyk <fedek6@gmail.com>
2021-03-29 23:50:05 +0200

    Fix express middlewares

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,15 +1,16 @@
 import express from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
 import DefaultRouter from "./DefaultRouter";
+import cors from "cors";
 
 const app = express();
 
 // Routes
-app.use(DefaultRouter);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("public"));
-app.use(cors);
-app.use(bodyParser.json);
+app.use(cors());
+app.use(DefaultRouter);
+
 
 app.listen(process.env.NODE_LOCAL_PORT, () => {
   console.log("Server running on port " + process.env.NODE_LOCAL_PORT);
