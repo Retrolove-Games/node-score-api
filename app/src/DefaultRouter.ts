63d34d4abb4737c8e01c8f31a51a87fcbe9fb9b7
Konrad Fedorczyk <fedek6@gmail.com>
2021-03-30 11:07:04 +0200

    Add get scores api route

--- app/src/DefaultRouter.ts
+++ app/src/DefaultRouter.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import path from "path";
-import { addProject, addScore, getResults } from "./db/dbInterface";
+import { addScore, getResults } from "./db/dbInterface";
 
 const DefaultRouter = Router();
 
@@ -9,10 +9,23 @@ DefaultRouter.get("/", (request, response) => {
   response.sendFile(path.join(__dirname, "/../public/index.html"));
 });
 
+// Post score
 DefaultRouter.post("/score/:name", (request, response) => {
   addScore(request.params.name, request.body.data)
     .then(() => {
       response.json({status: "OK"});
+    })
+    .catch(() => {
+      response.status(400).json({status: "Error"});
+    });
+});
+
+// get scores
+DefaultRouter.get("/score/:name/:count?", (request, response) => {
+
+  getResults(request.params.name, +request.params.count || 10)
+    .then(data => {
+      response.json(data);
     })
     .catch(() => {
       response.status(400).json({status: "Error"});
