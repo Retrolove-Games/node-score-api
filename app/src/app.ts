ed8727861d8acd01ae0869cb08264e47fc1be8d1
Konrad Fedorczyk <contact@realhe.ro>
2021-03-31 14:00:29 +0200

    Remove commented code

--- app/src/app.ts
+++ app/src/app.ts
@@ -11,29 +11,6 @@ app.use(express.static("public"));
 app.use(cors());
 app.use(DefaultRouter);
 
-
 app.listen(process.env.NODE_LOCAL_PORT, () => {
   console.log("Server running on port " + process.env.NODE_LOCAL_PORT);
 });
-
-/* app.get('/', (req, res, next) => {
-  res.json({hello: "world!!!", current_env: process.env.NODE_ENV});
-}); */
-
-
-/*
-import { Project } from "./db/models/Project";
-import db, { status } from "./db/Sequelize"; */
-/*
-app.get('/db', (req, res, next) => {
-  const project = new Project({ name: "test" });
-  project
-    .save()
-    .then(() => {
-      res.json({db: process.env.MYSQL_DATABASE, status: status()});
-    })
-    .catch((err) => {
-      res.status(500).json({ error: "DB connection problem" });
-    });
-});
-*/
